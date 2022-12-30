45c6ed9d9b5d5c49adba46632804b644977263ef
saiprakashk-387 <prakash387@yahoo.com>
2022-12-30 14:37:50 +0530

    code cleanup

--- Client/src/API/Config.jsx
+++ Client/src/API/Config.jsx
@@ -1,4 +1,4 @@
-export const base_url= "http://localhost:2000";
+export const base_url= "http://localhost:2050";
 export const ACCESS_TOKEN = () => sessionStorage.getItem('token');
 
 export function updateSessionInfo(value){
@@ -10,21 +10,3 @@ export function updateSessionInfo(value){
     // window.location.reload()
 }
 
-// axios.defaults.baseURL = `http://localhost:3300/`;
-
-// axios.defaults.headers.common = {
-//   Accept: "application/json",
-//   "Content-Type": "application/json",
-// };
-
-// const authHeader = (token) => ({
-//     ...(token && { Authorization: `Bearer ${token}` }),
-//   });
-  
-// getApiwithAuth: async (url, accessToken) => {
-//     // console.log("url",url);
-//     const headers = authHeader(accessToken);
-//     return await axios.get(url, {
-//       headers: headers,
-//     });
-//   },
